dd9f679b65dbf2007932ef74968cf0d9ad149ce7
kverret05 <97851573+kverret05@users.noreply.github.com>
2022-04-30 20:55:38 -0400

    Update databaseTable.sql

--- databaseTable.sql
+++ databaseTable.sql
@@ -9,7 +9,7 @@ contact_id INT(11),
     affiliation VARCHAR (255),
 PRIMARY KEY(contact_id)
 );
-INSERT INTO contactsList.book (contact_id, contact_fName, contact_lName, email, email, tel, affiliation) VALUES(0, 'Akiyama','Mizuki','amizuki@gmail.com','123-456-7890', 'family');
-INSERT INTO contactsList.book (contact_id, contact_fName, contact_lName, email, email, tel, affiliation) VALUES(1, 'Shinonone','Ena','sena@gmail.com','123-456-7890', 'family');
-INSERT INTO contactsList.book (contact_id, contact_fName, contact_lName, email, email, tel, affiliation) VALUES(2, 'Tenma','Tsukasa','ttsukasa@gmail.com','123-456-7890', 'family');
-INSERT INTO contactsList.book (contact_id, contact_fName, contact_lName, email, email, tel, affiliation) VALUES(3, 'Kusanagi','Nene','knene@gmail.com','123-456-7890', 'family');
+INSERT INTO contactsList.book (contact_id, contact_fName, contact_lName, email, tel, affiliation) VALUES(0, 'Akiyama','Mizuki','amizuki@gmail.com','123-456-7890', 'family');
+INSERT INTO contactsList.book (contact_id, contact_fName, contact_lName, email, tel, affiliation) VALUES(1, 'Shinonone','Ena','sena@gmail.com','123-456-7890', 'family');
+INSERT INTO contactsList.book (contact_id, contact_fName, contact_lName, email, tel, affiliation) VALUES(2, 'Tenma','Tsukasa','ttsukasa@gmail.com','123-456-7890', 'family');
+INSERT INTO contactsList.book (contact_id, contact_fName, contact_lName, email, tel, affiliation) VALUES(3, 'Kusanagi','Nene','knene@gmail.com','123-456-7890', 'family');
